21fcad36324fb98bdb64739c683366e8859ee2c7
Rakhi Shah <rakhisshah@gmail.com>
2020-01-28 14:32:04 +0100

    initial commit

---
+++ public/js/scripts.js
@@ -0,0 +1,6 @@
+function finfBook(){
+    document.getElementById('result').innerHTML='Book results will display here';
+    var userSearch=document.getElementById('userInput').value;
+    
+    document.getElementById('result').innerHTML=userSearch;
+}
---
+++ server.js
@@ -0,0 +1,17 @@
+const express= require('express');
+const exphbs=require('express-handlebars');
+const app=express();
+app.engine('handlebars', exphbs({
+    defaultLayout:'main'
+}));
+app.set('view engine','handlebars');
+app.use(express.static('public'));
+const port=3000;
+app.get('/',(req,res)=>{
+    res.render('home');
+});
+
+app.listen(port,()=>{
+    console.log('server is running on port') + port;
+
+})
